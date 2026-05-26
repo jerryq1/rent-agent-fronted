5783a174f27f3bb4e26ac1df7844e6dc301beef6
jerry <jerryxuq13@gmail.com>
2026-05-26 13:13:25 +0800

    feat: add vercel web analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { inject } from '@vercel/analytics'
+
+// Inject Vercel Web Analytics
+inject()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
